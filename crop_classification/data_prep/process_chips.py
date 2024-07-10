3be1de1fa14939af11739e7c62ae12bf42a9f1fc
SethDocherty <sddocherty@gmail.com>
2024-07-09 22:55:20 -0400

    Refactor after running through process_chip function

--- crop_classification/data_prep/process_chips.py
+++ crop_classification/data_prep/process_chips.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import json
 
 
-# The code cell is used to add the src directory to the Python path, making
+# The code used to add the src directory to the Python path, making
 # it possible to import modules from that directory.
 
 module_path = module_path = os.path.abspath(Path(__file__).parent.parent.resolve())
@@ -22,39 +22,47 @@ except ModuleNotFoundError:
     pass
 
 
+# Constants
+BANDS = ["B02", "B03", "B04", "B8A", "B11", "B12"]
+## set up CDL reclass
+cdl_class_df = pd.read_csv(CLD_RECLASS_PROPERTIES)
+CROP_DICT = dict(zip(cdl_class_df.old_class_value, cdl_class_df.new_class_value))
+
+
 def crop_multi(x):
-    return crop_dict[x]
+    return CROP_DICT[x]
 
 
-def get_tile_info(chip_tile, track_csv):
-    tile_info_df = track_csv[track_csv.tile == chip_tile]
-    selected_image_folders = tile_info_df.save_path.to_list()
-    assert len(selected_image_folders) == 3
+def get_tile_info(chip_tile, all_tiles_df):
+    tile_info_df = all_tiles_df[all_tiles_df.tile_id == chip_tile]
+    # Ensure that we have 3 images for the tile
+    assert len(tile_info_df) == 3
     first_image_date = tile_info_df.iloc[0].date
     second_image_date = tile_info_df.iloc[1].date
     third_image_date = tile_info_df.iloc[2].date
     return (
         tile_info_df,
-        selected_image_folders,
         first_image_date,
         second_image_date,
         third_image_date,
     )
 
 
-def get_image_paths(tile_info_df, bands):
+def get_image_paths(tile_info_df):
     all_date_images = []
     all_date_qa = []
+
     for i in range(3):
-        for band in bands:
+        filename = tile_info_df.iloc[i].title_id
+
+        # Get the paths to the images for each band
+        for band in BANDS:
             all_date_images.append(
-                tile_info_df.iloc[i].save_path
-                + f"{tile_info_df.iloc[i].filename}.{band}.reproject.tif"
+                Path(TILE_REPROJECTED_DIR) / f"{filename}.{band}.tif"
             )
-        all_date_qa.append(
-            tile_info_df.iloc[i].save_path
-            + f"{tile_info_df.iloc[i].filename}.Fmask.reproject.tif"
-        )
+
+        # Add the path to the QA band for each date
+        all_date_qa.append(Path(TILE_REPROJECTED_DIR) / f"{filename}.Fmask.tif")
     return all_date_images, all_date_qa
 
 
@@ -103,7 +111,6 @@ def check_qa(
         vals = out_image.flatten()
         unique, counts = np.unique(vals, return_counts=True)
         qa_df = pd.DataFrame({"qa_val": unique, "counts": counts})
-        qa_df
         qa_df[~qa_df.qa_val.isin(valid_qa)].sort_values(["counts"], ascending=False)
         qa_df["pct"] = (100 * qa_df["counts"]) / (224.0 * 224.0)
 
@@ -127,12 +134,12 @@ def get_out_bands(all_date_images, shape):
             out_meta = src.meta
             out_bands.append(out_image[0])
     out_bands = np.array(out_bands)
-    na_count = sum(out_bands.flatten() == -1000)
-    out_bands = np.clip(out_bands, 0, None)
-    return out_bands, out_transform, out_meta, na_count
+    # na_count = sum(out_bands.flatten() == -1000)
+    # out_bands = np.clip(out_bands, 0, None)
+    return out_bands, out_transform, out_meta
 
 
-def write_hls_chips(chip_id, out_bands, out_transform, out_meta, chip_dir):
+def write_hls_chips(chip_id, out_bands, out_transform, out_meta):
     out_meta.update(
         {
             "driver": "GTiff",
@@ -142,13 +149,18 @@ def write_hls_chips(chip_id, out_bands, out_transform, out_meta, chip_dir):
             "transform": out_transform,
         }
     )
+
+    na_count = sum(out_bands.flatten() == -1000)
+    out_bands = np.clip(out_bands, 0, None)
     with rasterio.open(
-        chip_dir + str(chip_id) + "_merged.tif", "w", **out_meta
+        Path(CHIP_DIR) / f"{chip_id}_merged.tif", "w", **out_meta
     ) as dest:
         dest.write(out_bands)
 
+    return out_meta, na_count
 
-def write_qa_bands(chip_id, qa_bands, out_transform, out_meta, chip_fmask_dir):
+
+def write_qa_bands(chip_id, qa_bands, out_transform, out_meta):
     out_meta.update(
         {
             "driver": "GTiff",
@@ -159,52 +171,49 @@ def write_qa_bands(chip_id, qa_bands, out_transform, out_meta, chip_fmask_dir):
         }
     )
     with rasterio.open(
-        chip_fmask_dir + str(chip_id) + "_Fmask.tif", "w", **out_meta
+        Path(FMASK_DIR) / f"{chip_id}_Fmask.tif", "w", **out_meta
     ) as dest:
         dest.write(qa_bands)
 
+    # return out_meta
 
-def clip_cdl_to_chip(cdl_file, shape):
-    with rasterio.open(cdl_file) as src:
+
+def clip_cdl_to_chip(shape):
+    with rasterio.open(CDL_SOURCE) as src:
         out_image, out_transform = rasterio.mask.mask(src, shape, crop=True)
         out_meta = src.meta
         colormap = src.colormap(1)
     return out_image, out_transform, out_meta, colormap
 
 
-def write_cdl_chips(
-    chip_id, out_image_multi, out_transform, out_meta, colormap, chip_dir
-):
+def write_cdl_chips(chip_id, out_image, out_transform, out_meta, colormap):
     out_meta.update(
         {
             "driver": "GTiff",
-            "height": out_image_multi.shape[1],
-            "width": out_image_multi.shape[2],
+            "height": out_image.shape[1],
+            "width": out_image.shape[2],
             "transform": out_transform,
         }
     )
-    with rasterio.open(chip_dir + str(chip_id) + ".mask.tif", "w", **out_meta) as dest:
+    c_multi = np.vectorize(crop_multi)
+    out_image_multi = c_multi(out_image).astype(np.uint8)
+    with rasterio.open(Path(CHIP_DIR) / f"{chip_id}.mask.tif", "w", **out_meta) as dest:
         dest.write(out_image_multi)
         dest.write_colormap(1, colormap)
 
 
-def process_chip(
-    chip_id,
-    chip_tile,
-    shape,
-    track_csv,
-    cdl_file,
-    bands=["B02", "B03", "B04", "B8A", "B11", "B12"],
-):
+def process_chip(chip_id, chip_tile, shape, all_tiles):
+
+    # Select a tile and check to ensure that the tile has 3 images
     (
-        tile_info_df,
-        selected_image_folders,
+        selected_tile,
         first_image_date,
         second_image_date,
         third_image_date,
-    ) = get_tile_info(chip_tile, track_csv)
+    ) = get_tile_info(chip_tile, all_tiles)
 
-    all_date_images, all_date_qa = get_image_paths(tile_info_df, bands)
+    # Get the paths to the images and QA files for the tile
+    all_date_images, all_date_qa = get_image_paths(selected_tile)
 
     (
         valid_first,
@@ -219,15 +228,17 @@ def process_chip(
         qa_bands,
     ) = check_all_qa(all_date_qa, shape)
 
-    out_bands, out_transform, out_meta, na_count = get_out_bands(all_date_images, shape)
+    out_bands, out_transform, ouput_metadata = get_out_bands(all_date_images, shape)
 
-    write_hls_chips(chip_id, out_bands, out_transform, out_meta, CHIP_DIR)
+    ouput_metadata, na_count = write_hls_chips(
+        chip_id, out_bands, out_transform, ouput_metadata
+    )
 
-    write_qa_bands(chip_id, qa_bands, out_transform, out_meta, FMASK_DIR)
+    write_qa_bands(chip_id, qa_bands, out_transform, ouput_metadata)
 
-    out_image, out_transform, out_meta, colormap = clip_cdl_to_chip(cdl_file, shape)
+    out_image, out_transform, ouput_metadata, colormap = clip_cdl_to_chip(shape)
 
-    write_cdl_chips(chip_id, out_image, out_transform, out_meta, colormap, CHIP_DIR)
+    write_cdl_chips(chip_id, out_image, out_transform, ouput_metadata, colormap)
 
     return {
         "valid_first": valid_first,
@@ -243,11 +254,6 @@ def process_chip(
     }
 
 
-def c_multi(out_image):
-    # Add your implementation of the c_multi function here
-    np.vectorize(crop_multi)
-
-
 def main():
 
     # Define missing variables
@@ -258,40 +264,45 @@ def main():
     track_df = None
     ## process chips
     failed_tiles = []
+    chip_data = []
 
     ## set up CDL reclass
     cdl_class_df = pd.read_csv(CLD_RECLASS_PROPERTIES)
     crop_dict = dict(zip(cdl_class_df.old_class_value, cdl_class_df.new_class_value))
 
-    failed_tiles = []
-    chip_data = []
-
+    # Load in details about the chips
     chip_df = pd.read_pickle(CHIPS_DF_PKL)
     with open(CHIPS_ID_JSON) as f:
         chip_ids = json.load(f)
-    track_df = pd.read_pickle(TILES_DF_PKL)
     with open(BB_CHIP_PAYLOAD, "r") as file:
         chips = json.load(file)
-
-    tiles_to_chip = track_df.tile_id.unique().tolist()
     with open(BB_CHIP_5070_PAYLOAD, "r") as file_chip:
         chipping_js = json.load(file_chip)
 
+    # Load in the selected tiles that were reprojected and get a list of the tiles to chip
+    selected_tiles_df = pd.read_pickle(SELECTED_TILES_PKL)
+    tiles_to_chip = selected_tiles_df.tile_id.unique().tolist()
+
     for tile in tiles_to_chip:
+        # Tiles contain the prefix 'T' in the chip_df, so we need to remove it
+        # and filter the chips to process by the tile
         chips_to_process = chip_df[chip_df.tile == tile[1:]].reset_index(drop=True)
         for k in range(len(chips_to_process)):
-            current_id = chips_to_process.chip_id[k]
-            chip_tile = chips_to_process.tile[k]
-            chip_index = chip_ids.index(current_id)
 
+            # Get the chip_id e.g. `chip_184_236` as to identify the index in the chips json
+            # and extract the chip details to be processed
+            current_id = chips_to_process.chip_id[k]
+            chip_index = chip_ids.index(current_id)
             chip_feature = chipping_js["features"][chip_index]
 
+            # Grab the tile that overlaps with the chip and the geometry shape of the chip
+            chip_tile = chips_to_process.tile[k]
             shape = [chip_feature["geometry"]]
             full_tile_name = "T" + chip_tile
 
             try:
                 chip_info = process_chip(
-                    current_id, full_tile_name, shape, track_df, CDL_SOURCE
+                    current_id, full_tile_name, shape, selected_tiles_df
                 )
                 chip_info["chip_id"] = current_id
                 chip_data.append(chip_info)
