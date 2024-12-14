09dc286feba1e47358157052225a5b35517bae1d
SethDocherty <sddocherty@gmail.com>
2024-12-13 22:39:13 -0500

    Refactored to split up methods to only focus on retrieving content from IPFS

--- crop_classification/data_prep/scripts/ipfs_cli_download.py
+++ crop_classification/data_prep/scripts/ipfs_cli_download.py
@@ -18,7 +18,7 @@ except ModuleNotFoundError:
 
 # Pull out all parameters from `IPFS_STAC` that are not empty or None and create a client object
 params = {k: v for k, v in IPFS_STAC.items() if v}
-easier = client.Web3(**params)
+# easier = client.Web3(**params)
 
 
 def retrieve_assets(data_payload: Dict) -> None:
@@ -53,98 +53,34 @@ def retrieve_assets(data_payload: Dict) -> None:
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE,
                 )
-                print(result.stdout)
-
-
-def create_tile_payload(easier: client.Web3, tile_info: Dict) -> Union[Dict, None]:
-    """
-    Creates a payload for a tile by searching for the tile in the STAC catalog and retrieving assets for specified bands.
-    Args:
-        easier (client.Web3): An instance of the Web3 client used to interact with the STAC catalog.
-        tile_info (Dict): A dictionary containing information about the tile, including the "title_id".
-    Returns:
-        Union[Dict, None]: A list of dictionaries, each containing information about a band asset, or None if the tile is not found.
-            Each dictionary in the list contains:
-                - "filename" (str): The filename of the band asset.
-                - "cid" (str): The content identifier (CID) of the band asset.
-                - "band_name" (str): The name of the band.
-    """
-
-    title_id = tile_info["title_id"]
-    tile_payload = []
-
-    try:
-        item = easier.searchSTAC(ids=[title_id])
-        # If the item is not found, return an empty dictionary
-        if not item:
-            print(f"\nThe item, {title_id}, does not exist in the STAC catalog.")
-            return None
-
-        for band in HLS_BANDS:
-            try:
-                asset = easier.getAssetFromItem(item[0], band)
-                cid = str(asset)[:59]
-                asset.cid = cid
-                band_payload = {
-                    "filename": f"{title_id}.{band}.tif",
-                    "cid": asset.cid,
-                    "band_name": band,
-                }
-                tile_payload.append(band_payload)
-            except Exception as e:
-                print(f"Asset error for {band}: {str(e)}")
-    except Exception as e:
-        print(f"Tile error: {str(e)}")
-
-    return tile_payload
+                if result.returncode != 0:
+                    print(f"Error downloading {cid}")
+                    print(result.stderr.decode("utf-8"))
+                else:
+                    print(result.stdout.decode("utf-8"))
 
 
 def main() -> None:
     """
     Main function to read selected tiles, create payloads, and retrieve assets.
+
     This function performs the following steps:
-    1. Reads the selected tiles from a CSV file.
-    2. Creates a JSON file to store tile payload information for each tile.
-    3. Processes each tile to create its payload and stores it in a dictionary.
-    4. Saves the tile payloads to a JSON file.
-    5. Retrieves the assets for each tile using the payload information.
-    If the selected tiles CSV file is not found, an error message is printed and the function returns.
+    1. Reads tile payloads to a JSON file.
+    2. Retrieves the assets for each tile using the payload information.
+
     If the tile payloads JSON file is not found, an error message is printed and the function returns.
     """
-    # Read the selected tiles
-    try:
-        tiles_df = pd.read_csv(SELECTED_TILES_CSV)
-    except FileNotFoundError:
-        print(f"Error: Could not find {SELECTED_TILES_CSV}")
-        return
+    # Read in the output from running the `grab_cids_from_selected_tiles.py` script
+    tile_payloads_file = Path(MISC_DIR, "tile_payloads.json").resolve()
+    if not tile_payloads_file.exists():
+        raise FileNotFoundError(f"Could not find {tile_payloads_file}")
 
-    # Create a json file to store tile payload information for each tile
-    all_tile_payloads = {}
-    # Process each tile
-    for _, tile_info in tqdm.tqdm(tiles_df.iterrows(), total=len(tiles_df)):
-        tile_payload = create_tile_payload(easier, tile_info)
-        if tile_payload:
-            all_tile_payloads[tile_info.title_id] = tile_payload
+    # open json file
+    with open(tile_payloads_file.as_posix(), "r") as file:
+        all_tile_payloads = json.load(file)
 
-    # # Save the tile payloads to a JSON file
-    tile_payloads_file = Path(MISC_DIR, "tile_payloads.json").resolve().as_posix()
-    with open(tile_payloads_file, "w") as f:
-        json.dump(all_tile_payloads, f)
-    try:
-        if not all_tile_payloads:
-            with open(
-                tile_payloads_file,
-                "r",
-            ) as file:
-                all_tile_payloads = json.load(file)
-        else:
-            raise FileNotFoundError()
-
-        # Retrieve the assets for each tile
-        retrieve_assets(all_tile_payloads)
-    except FileNotFoundError as e:
-        print(f"Error: Unable to continue - {str(e)}")
-        return
+    # Retrieve the assets for each tile
+    retrieve_assets(all_tile_payloads)
 
 
 if __name__ == "__main__":
